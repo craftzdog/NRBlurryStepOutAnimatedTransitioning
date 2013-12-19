e162a8ce0c2f1dce9643f45ef998a3ccd69631de
noradaiko <nora@odoruinu.net>
2013-12-20 00:37:10 +0900

    Initial Commit

---
+++ BlurredStepOutModalSample/NRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NRAppDelegate.h
+//  BlurredStepOutModalSample
+//
+//  Created by nora on 12/20/13.
+//  Copyright (c) 2013 PuKaPuKa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlurredStepOutModalSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlurredStepOutModalSample
+//
+//  Created by nora on 12/20/13.
+//  Copyright (c) 2013 PuKaPuKa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NRAppDelegate class]));
+    }
+}
